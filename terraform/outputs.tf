2b75319f93f72c6bd63e3647ff5f5831b29f2f73
cxnnqr <connor.neubauer@icloud.com>
2025-02-04 14:23:44 +0100

    refactoring: removed debugging outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,23 +7,3 @@ output "frontend_vip_addr" {
   value       = module.loadbalancer.frontend_vip_addr
   description = "IP address of the frontend load balancer"
 }
-
-output "backend_private_IPs" {
-  value       = module.compute.backend_private_ip_list
-  description = "list of private IP addresses of the backend instances"
-}
-
-output "backend_instance_names" {
-  value       = module.compute.backend_instance_names
-  description = "list of names of the backend instances"
-}
-
-output "frontend_private_IPs" {
-  value       = module.compute.frontend_private_ip_list
-  description = "list of private IP addresses of the frontend instances"
-}
-
-output "frontend_instance_names" {
-  value       = module.compute.frontend_instance_names
-  description = "list of names of the frontend instances"
-}
