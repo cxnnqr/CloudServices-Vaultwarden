be742afc066fa93bc1990dd134a7817f08f5bea2
cxnnqr <connor.neubauer@icloud.com>
2025-02-05 14:20:56 +0100

    output for delpoyment floating IP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "frontend_vip_addr" {
   value       = module.loadbalancer.frontend_vip_addr
   description = "IP address of the frontend load balancer"
 }
+
+output "deployment_floating_ip" {
+  value       = module.compute.deployment_floating_ip
+  description = "floating IP of the deployment instance"
+}
