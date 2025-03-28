5396ab1dd545059689c8eb9f66c6e5e85409e979
maximizzar <60427093+maximizzar@users.noreply.github.com>
2025-03-28 16:13:16 +0100

    feat: made vault role non-interactive

---
+++ vault.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ ! -f "./ansible.cfg" ]; then
+    echo "Run at the root of the git repo!"
+fi
+
+if [ ! -f ./"group_vars/all/secrets.yml" ]; then
+    echo "${ANSIBLE_VAULT_PASSWORD}" > /tmp/.vault_pass
+fi
